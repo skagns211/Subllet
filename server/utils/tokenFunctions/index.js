f996fef695681246977930d56536a2fdad3cce25
steve <ionc635@naver.com>
2021-11-30 01:27:27 +0900

    Feat: modify signup, login API
    bcrypt module을 사용해 password를 해시 처리했다.
    redis를 활용한 로그인 방식을 구현하고 있다.

---
+++ server/utils/tokenFunctions/index.js
@@ -0,0 +1,24 @@
+const { sign, verify } = require('jsonwebtoken');
+const redisClient = require("../redis");
+require("dotenv").config();
+
+module.exports = {
+    generateAccessToken: (data) => {
+        return sign(data, process.env.ACCESS_SECRET, { expiresIn: "30m"})
+    },
+    generateRefreshToken: () => {
+        return sign({}, process.env.REFRESH_SECRET, { expiresIn: '1d'})
+    },
+    isAuthorized: (req) => {
+        const authorization = req.headers["authorization"];
+        if (!authorization) {
+            return null;
+        }
+        const token = authorization.split(" ")[1];
+        try {
+            return verify(token, process.env.ACCESS_SECRET);
+        } catch (err) {
+            return null;
+        }
+    }
+}
