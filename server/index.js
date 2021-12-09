27644ced0724d733d57158adbc65768afd10e2fd
namhunKim <skagns211@gmail.com>
2021-12-10 03:09:06 +0900

    Fix: code Fix Etc..

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
   })
@@ -22,7 +22,7 @@ app.use(
 // app.use(cookieParser());
 app.use("/", indexRouter);
 
-// const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 
 // const port = 4000;
 
@@ -30,16 +30,16 @@ app.use("/", indexRouter);
 //   console.log(`run http://localhost:${port}`);
 // });
 
-// let server;
-// if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-//   const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-//   const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-//   const credentials = { key: privateKey, cert: certificate };
+let server;
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
 
-//   server = https.createServer(credentials, app);
-//   server.listen(HTTPS_PORT, () => console.log("server runnning"));
-// } else {
-//   server = app.listen(HTTPS_PORT);
-// }
+  server = https.createServer(credentials, app);
+  server.listen(HTTPS_PORT, () => console.log("server runnning"));
+} else {
+  server = app.listen(HTTPS_PORT);
+}
 
-module.exports = app;
+module.exports = server;
